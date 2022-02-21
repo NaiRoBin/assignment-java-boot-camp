acc0032729ba1b893bfad02782a2a0478d8d9fbf
Chanapat Rassamee <chanapat.ras@gmail.com>
2022-02-21 21:24:52 +0700

    inital Commit initial Project with Spring initializr #1

---
+++ src/main/java/com/javabootcamp/crshop/CrshopApplication.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.crshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javabootcamp/crshop/CrshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.crshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
