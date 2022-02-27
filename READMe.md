c0a91163f6cbb13d123e933a9316112e4d426b7a
Chanapat Rassamee <chanapat.ras@gmail.com>
2022-02-27 20:25:55 +0700

    Doc : Update READMe.md #8
    Add List
    * Design Flow and Restful API
    * Run Command
    * Test on Browser
    
    Refs : Issue#8

--- READMe.md
+++ READMe.md
@@ -12,3 +12,12 @@
 * java -jar target/crshop-0.0.1-SNAPSHOT.jar
 
 
+## Test on Browser
+| Method | Url                            | Description                | Url Test                                     |Request Body|
+|:-------|:-------------------------------|:---------------------------|:---------------------------------------------|:------------ |
+| Get    | /api/user/{name}               | Find User By Name          | http://localhost:8080/api/user/pom           |-|
+| Get    | /api/product                   | Find All Product           | http://localhost:8080/api/product/           |-|
+| Get    | /api/product/{name}            | Find Product By Name       | http://localhost:8080/api/product/product001 |-|
+| Get    | /api/basket/{userid}           | Find basket from userid    | http://localhost:8080/api/basket/4           |-|
+| Post   | /api/basket/add                | Add Product to basket      | http://localhost:8080/api/basket/add         |{"userId":4,"productId":6,<br/>"productName":null,"amount":1,<br/>"price":150.0,"sumprice":null}|
+
--- src/main/java/com/javabootcamp/crshop/CrshopApplication.java
+++ src/main/java/com/javabootcamp/crshop/CrshopApplication.java
@@ -1,5 +1,7 @@
 package com.javabootcamp.crshop;
 
+import com.javabootcamp.crshop.basket.Basket;
+import com.javabootcamp.crshop.basket.BasketRepository;
 import com.javabootcamp.crshop.product.Product;
 import com.javabootcamp.crshop.product.ProductRepository;
 import com.javabootcamp.crshop.users.User;
@@ -17,6 +19,8 @@ public class CrshopApplication {
 	private UserRepository userRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private BasketRepository basketRepository;
 
 	public CrshopApplication() {
 	}
@@ -43,6 +47,54 @@ public class CrshopApplication {
 		product.setPrice(200F);
 		product.setQuantity(10);
 		productRepository.save(product);
+
+		Product product2 = new Product();
+		product2.setName("product002");
+		product2.setDescription("product002 description");
+		product2.setPrice(150F);
+		product2.setQuantity(10);
+		productRepository.save(product2);
+	}
+
+	@PostConstruct
+	public void addBasket(){
+		User user = new User();
+		user.setName("pom");
+		user.setEmail("test@gmail.com");
+		user.setUsername("pom");
+		user.setDistrict("บางแค");
+		user.setProvince("กรุงเทพมหานคร");
+		userRepository.save(user);
+
+		System.out.println("User id : "+user.getId());
+
+		Product product = new Product();
+		product.setName("product001");
+		product.setDescription("product001 description");
+		product.setPrice(200F);
+		product.setQuantity(10);
+		productRepository.save(product);
+
+		Product product2 = new Product();
+		product2.setName("product002");
+		product2.setDescription("product002 description");
+		product2.setPrice(150F);
+		product2.setQuantity(10);
+		productRepository.save(product2);
+
+		Basket basket = new Basket();
+		basket.setAmount(1);
+		basket.setPrice(product.getPrice());
+		basket.setProductId(product.getId());
+		basket.setUserId(user.getId());
+		basketRepository.save(basket);
+
+		Basket basket2 = new Basket();
+		basket2.setAmount(1);
+		basket2.setPrice(product2.getPrice());
+		basket2.setProductId(product2.getId());
+		basket2.setUserId(user.getId());
+		basketRepository.save(basket2);
 	}
 
 	public static void main(String[] args) {
